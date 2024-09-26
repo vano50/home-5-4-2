16e63ada7880e720773dce11a9256ec7964d8b95
Vano <vano50@mail.ru>
2024-09-26 16:07:29 +0300

    Initial commit

---
+++ home-5-4-2/home-5-4-2.cpp
@@ -0,0 +1,25 @@
+﻿#include <iostream>
+#include <set>
+
+
+int main() {
+    int z;
+    std::cout << "[IN]: " << std::endl;
+    std::cin >> z;
+
+    std::set<int> mySet;
+
+    for (int i = 0; i < z; ++i) {
+        int number;
+        std::cin >> number;
+        mySet.insert(number);
+
+    }
+
+    std::cout << "[OUT]: " << " ";
+    for (auto x = mySet.rbegin(); x != mySet.rend(); ++x) {
+        std::cout << *x << " ";
+    }
+
+    return 0;
+}
